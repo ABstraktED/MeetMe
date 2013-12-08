d7d9785ad608fcf682aa83b5ee238dfe675a6d75
dirstons <dirstons@gmail.com>
2013-12-08 04:01:03 +0100

    Add constrainst to User.groovy

--- App/Web/MeetMe/grails-app/domain/pwr/itapp/meetme/auth/User.groovy
+++ App/Web/MeetMe/grails-app/domain/pwr/itapp/meetme/auth/User.groovy
@@ -12,7 +12,7 @@ class User {
 	boolean accountExpired
 	boolean accountLocked
 	boolean passwordExpired
-	
+
 	String email
 	String name
 	String phone
@@ -25,6 +25,14 @@ class User {
 	static constraints = {
 		username blank: false, unique: true, nullable: false
 		password blank: false, nullable: false
+
+		email email: true, blank: false, unique: true
+		name size: 2..30, blank: false, unique: false
+		phone blank: false, unique: false //, matches: "/[+]([0-9]{2})([ -]?)([0-9]{3})([ -]?)([0-9]{3})([ -]?)([0-9]{3})/"
+		// to check regex visit http://www.regexper.com/#%2F%5B%2B%5D(%5B0-9%5D%7B2%7D)(%5B%20-%5D%3F)(%5B0-9%5D%7B3%7D)%5C2(%5B0-9%5D%7B3%7D)%5C2(%5B0-9%5D%7B3%7D)%2F
+		status blank: false, unique: false
+		image blank: true
+		location blank: true
 	}
 
 	static mapping = {

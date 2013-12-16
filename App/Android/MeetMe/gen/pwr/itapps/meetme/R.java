be53d5e5e9f7a1d99471f2b71046a3b620ea0f49
holuss <holuss@gmail.com>
2013-12-16 22:11:10 +0100

    Main board (wall) and some data structures

--- App/Android/MeetMe/gen/pwr/itapps/meetme/R.java
+++ App/Android/MeetMe/gen/pwr/itapps/meetme/R.java
@@ -8,6 +8,9 @@
 package pwr.itapps.meetme;
 
 public final class R {
+    public static final class array {
+        public static final int menu_options=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -21,50 +24,75 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int action_search=0x7f020000;
+        public static final int drawer_shadow=0x7f020001;
+        public static final int earth=0x7f020002;
+        public static final int empty=0x7f020003;
+        public static final int error=0x7f020004;
+        public static final int event_background=0x7f020005;
+        public static final int ic_drawer=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
+        public static final int jupiter=0x7f020008;
+        public static final int loading=0x7f020009;
+        public static final int mars=0x7f02000a;
+        public static final int mercury=0x7f02000b;
+        public static final int neptune=0x7f02000c;
+        public static final int saturn=0x7f02000d;
+        public static final int uranus=0x7f02000e;
+        public static final int venus=0x7f02000f;
     }
     public static final class id {
-        public static final int action_forgot_password=0x7f08000a;
-        public static final int action_settings=0x7f08000b;
-        public static final int email=0x7f080003;
-        public static final int login=0x7f080005;
-        public static final int login_form=0x7f080002;
-        public static final int login_status=0x7f080000;
-        public static final int login_status_message=0x7f080001;
-        public static final int pager=0x7f080007;
-        public static final int pager_title_strip=0x7f080008;
-        public static final int password=0x7f080004;
-        public static final int section_label=0x7f080009;
-        public static final int sign_in_button=0x7f080006;
+        public static final int action_forgot_password=0x7f09000f;
+        public static final int action_settings=0x7f090010;
+        public static final int action_websearch=0x7f090011;
+        public static final int content_frame=0x7f090008;
+        public static final int drawer_layout=0x7f090007;
+        public static final int email=0x7f090003;
+        public static final int event_row_date_tv=0x7f09000d;
+        public static final int event_row_image_iv=0x7f09000a;
+        public static final int event_row_organizators_tv=0x7f09000c;
+        public static final int event_row_title_tv=0x7f09000b;
+        public static final int image=0x7f09000e;
+        public static final int left_drawer=0x7f090009;
+        public static final int login=0x7f090005;
+        public static final int login_form=0x7f090002;
+        public static final int login_status=0x7f090000;
+        public static final int login_status_message=0x7f090001;
+        public static final int password=0x7f090004;
+        public static final int sign_in_button=0x7f090006;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int fragment_main_dummy=0x7f030002;
+        public static final int drawer_list_item=0x7f030002;
+        public static final int event_list=0x7f030003;
+        public static final int event_row=0x7f030004;
+        public static final int fragment_planet=0x7f030005;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int login=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f05000a;
+        public static final int action_forgot_password=0x7f05000b;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f050008;
-        public static final int action_sign_in_short=0x7f050009;
+        public static final int action_sign_in_register=0x7f050009;
+        public static final int action_sign_in_short=0x7f05000a;
+        public static final int action_websearch=0x7f050004;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f05000f;
-        public static final int error_incorrect_password=0x7f05000e;
-        public static final int error_invalid_email=0x7f05000c;
-        public static final int error_invalid_password=0x7f05000d;
-        public static final int login_progress_signing_in=0x7f05000b;
+        public static final int app_not_available=0x7f050005;
+        public static final int drawer_close=0x7f050003;
+        public static final int drawer_open=0x7f050002;
+        public static final int error_field_required=0x7f050010;
+        public static final int error_incorrect_password=0x7f05000f;
+        public static final int error_invalid_email=0x7f05000d;
+        public static final int error_invalid_password=0x7f05000e;
+        public static final int login_progress_signing_in=0x7f05000c;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050006;
-        public static final int prompt_password=0x7f050007;
-        public static final int title_activity_login=0x7f050005;
-        public static final int title_section1=0x7f050002;
-        public static final int title_section2=0x7f050003;
-        public static final int title_section3=0x7f050004;
+        public static final int prompt_email=0x7f050007;
+        public static final int prompt_password=0x7f050008;
+        public static final int title_activity_login=0x7f050006;
     }
     public static final class style {
         /** 
@@ -90,11 +118,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int LoginFormContainer=0x7f060002;
+        public static final int AppTheme=0x7f070001;
+        public static final int LoginFormContainer=0x7f070002;
     }
 }
